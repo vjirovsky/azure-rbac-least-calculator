910fccf0c4f204d15f21b3bad36d89cfcb8b0d9a
vjirovsky <vjirovsky@vjirovsky.cz>
2024-02-17 13:03:16 +0100

    fixed script

--- generate-json.sh
+++ generate-json.sh
@@ -1,3 +1,3 @@
-az role definition list --query "[?roleType=='BuiltInRole'].{id:name,roleName:roleName,permissions:permissions,type:type} | Out-File ./public/roles.json
+az role definition list --query "[?roleType=='BuiltInRole'].{id:name,roleName:roleName,permissions:permissions,type:type} > ./public/roles.json
 
 az provider operation list > ./public/permissions.json
