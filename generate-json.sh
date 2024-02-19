57bb425b3e5c0a80cae5ed22ffd6fd0e6a0c083b
Vaclav Jirovsky <vjirovsky@purestorage.com>
2024-02-19 14:15:57 +0100

    changed github workflow

--- generate-json.sh
+++ generate-json.sh
@@ -1,4 +1,2 @@
 az provider operation list > ./build/permissions.json
 az role definition list --query "[?roleType=='BuiltInRole'].{id:name,roleName:roleName,permissions:permissions,type:type}" > ./build/roles.json
-
-node extend-roles-data.js
