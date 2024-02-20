19d405bcfd842bf28a4f539b1a1abfee81a8c3d5
Vaclav Jirovsky <vjirovsky@purestorage.com>
2024-02-20 14:31:59 +0100

    debugging action

--- generate-json.sh
+++ generate-json.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+set -x
 az provider operation list > ./build/permissions.json
 az role definition list --query "[?roleType=='BuiltInRole'].{id:name,roleName:roleName,permissions:permissions,type:type}" > ./build/roles.json
 
 echo 'Files './build/permissions.json' and './build/roles.json'  generated successfully!'
 pwd
+set +x
