3be24c7f8fb6c4bc937f2acbd05c83a6d46ed085
Vaclav Jirovsky <vjirovsky@purestorage.com>
2024-02-20 14:45:02 +0100

    change GH action

--- generate-json.sh
+++ generate-json.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-set -x
+
 az provider operation list > ./build/permissions.json
 az role definition list --query "[?roleType=='BuiltInRole'].{id:name,roleName:roleName,permissions:permissions,type:type}" > ./build/roles.json
 
 echo 'Files './build/permissions.json' and './build/roles.json'  generated successfully!'
-pwd
-set +x
